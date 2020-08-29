4cfe8fce014b7726a10ed173b8bf645018f6b70a
Alex Komissarov <k.a.komissar@gmail.com>
2020-08-29 18:01:42 +0500

    |1,4
    *encoding=utf-8
    
    ~h
    :q
    |2,0,1598706123,,"q"

--- bins/restart_udevd.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-sudo systemctl stop systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
-sudo systemctl start systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
-
