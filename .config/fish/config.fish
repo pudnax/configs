174ec7b50c4fab5a43c04797ab67c9548e961231
Alex Komissarov <k.a.komissar@gmail.com>
2020-11-02 19:24:26 +0500

    Update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,6 +55,7 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 # fix "xdg-open fork-bomb" export your preferred browser from here
 export BROWSER=/usr/bin/firefox
 
+bind -M insert \t accept-autosuggestion
 set fish_greeting
 
 function _z_cd
