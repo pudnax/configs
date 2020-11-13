8c32a9355c101f4fceb9cbe935bfe79cd6ae0c42
Alex Komissarov <k.a.komissar@gmail.com>
2020-11-13 13:32:12 +0500

    Fix identation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -135,29 +135,29 @@ setenv FZF_DEFAULT_OPTS '--height 20%'
 
 function ex --description "Universal archive extractor script"
     for file in $argv
-            switch $file
-                case '*.tar.bz2'
-                    tar xjf $file
-                case '*.tar.gz'
-                    tar xzf $file
-                case '*bz2'
-                    bunzip2  $file
-                case '*.rar'
-                    unrar x  $file
-                case '*.tar'
-                    tar xf  $file
-                case '*.tbz2'
-                    tar xjf $file
-                case '*.tgz'
-                    tar xzf $file
-                case '*.zip'
-                    unzip  $file
-                case '*.Z'
-                    uncompress  $file
-                case '*.7z'
-                    7z x  $file
-                case '*'
-                    echo "$file cannot be extracted via ex()"
-            end
+        switch $file
+            case '*.tar.bz2'
+                tar xjf $file
+            case '*.tar.gz'
+                tar xzf $file
+            case '*bz2'
+                bunzip2  $file
+            case '*.rar'
+                unrar x  $file
+            case '*.tar'
+                tar xf  $file
+            case '*.tbz2'
+                tar xjf $file
+            case '*.tgz'
+                tar xzf $file
+            case '*.zip'
+                unzip  $file
+            case '*.Z'
+                uncompress  $file
+            case '*.7z'
+                7z x  $file
+            case '*'
+                echo "$file cannot be extracted via ex()"
         end
+    end
 end
