f58b3c6bc5d22ebbb7db638f537dd171b54a2fd9
pudi <k.a.komissar@gmail.com>
2021-10-18 21:05:07 +0500

    lsp: Fix autocommands for shaders

--- .config/nvim-lsp/lua/autocommands.lua
+++ .config/nvim-lsp/lua/autocommands.lua
@@ -1,21 +1,21 @@
 vim.cmd([[
-	autocmd Filetype glsl,wgls <C-K>" :pyf ~/bins/clang-format.py<cr>
+    autocmd Filetype glsl,wgsl nnoremap <C-k> <cmd>pyf ~/bins/clang-format.py<cr>
 
-	autocmd Filetype c,cpp nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('make ;read')<CR>
-	autocmd Filetype c,cpp nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('make run;read')<CR>
-	autocmd Filetype c,cpp nnoremap <leader>H <Cmd>ClangdSwitchSourceHeader<CR>
+    autocmd Filetype c,cpp nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('make ;read')<CR>
+    autocmd Filetype c,cpp nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('make run;read')<CR>
+    autocmd Filetype c,cpp nnoremap <leader>H <Cmd>ClangdSwitchSourceHeader<CR>
 
-	autocmd Filetype java nnoremap <leader>r <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='mvn package;read', hidden =false}:toggle()<CR>
-	autocmd Filetype java nnoremap <leader>m <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='mvn compile;read', hidden =false}:toggle()<CR>
+    autocmd Filetype java nnoremap <leader>r <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='mvn package;read', hidden =false}:toggle()<CR>
+    autocmd Filetype java nnoremap <leader>m <cmd>lua require('toggleterm.terminal').Terminal:new {cmd='mvn compile;read', hidden =false}:toggle()<CR>
 
-	autocmd Filetype python nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('python " .. vim.fn.expand("%") .. ";read')<CR>
-	autocmd Filetype python nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('echo \"compile :pepelaugh:\";read')<CR>
+    autocmd Filetype python nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('python " .. vim.fn.expand("%") .. ";read')<CR>
+    autocmd Filetype python nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('echo \"compile :pepelaugh:\";read')<CR>
 
-	autocmd Filetype rust nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('cargo build;read')<CR>
-	autocmd Filetype rust nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('cargo run;read')<CR>
-	autocmd Filetype rust nnoremap <leader>H <cmd>lua require('core.terminal').exec_toggle('cargo clippy;read')<CR>
-	autocmd Filetype rust nnoremap <leader>lm <Cmd>RustExpandMacro<CR>
-	autocmd Filetype rust nnoremap <leader>lH <Cmd>RustToggleInlayHints<CR>
-	autocmd Filetype rust nnoremap <leader>le <Cmd>RustRunnables<CR>
-	autocmd Filetype rust nnoremap <leader>lh <Cmd>RustHoverActions<CR>
+    autocmd Filetype rust nnoremap <leader>m <cmd>lua require('core.terminal').exec_toggle('cargo build;read')<CR>
+    autocmd Filetype rust nnoremap <leader>r <cmd>lua require('core.terminal').exec_toggle('cargo run;read')<CR>
+    	autocmd Filetype rust nnoremap <leader>H <cmd>lua require('core.terminal').exec_toggle('cargo clippy;read')<CR>
+    autocmd Filetype rust nnoremap <leader>lm <Cmd>RustExpandMacro<CR>
+    autocmd Filetype rust nnoremap <leader>lH <Cmd>RustToggleInlayHints<CR>
+    autocmd Filetype rust nnoremap <leader>le <Cmd>RustRunnables<CR>
+    autocmd Filetype rust nnoremap <leader>lh <Cmd>RustHoverActions<CR>
 ]])
